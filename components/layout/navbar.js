7888ee54ede46682cf19ec63e26222dd260be0f0
Hamza Sheraz <hamzasheraz429@gmail.com>
2024-10-20 15:20:58 +0500

    Dark mode removed

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -1,10 +1,12 @@
 import Navigation from './navigation';
+import ThemeToggle from './theme/theme';
 
-const Navbar = ({route,name}) => {
-  return (  
+const Navbar = ({ route, name }) => {
+  return (
     <header className="bg-info text-white py-4">
       <div className="container d-flex justify-content-between align-items-center">
         <h1 className="h2">Book Library</h1>
+        <ThemeToggle />
         <Navigation route={route} name={name} />
       </div>
     </header>
