a604e6c97c7ab3889c52319f20708930c3f4b3ce
Hamza Sheraz <hamzasheraz429@gmail.com>
2024-10-20 15:21:45 +0500

    Navbar fixed

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -1,12 +1,10 @@
 import Navigation from './navigation';
-import ThemeToggle from './theme/theme';
 
 const Navbar = ({ route, name }) => {
   return (
     <header className="bg-info text-white py-4">
       <div className="container d-flex justify-content-between align-items-center">
         <h1 className="h2">Book Library</h1>
-        <ThemeToggle />
         <Navigation route={route} name={name} />
       </div>
     </header>
